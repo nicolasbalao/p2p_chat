7f35c93e415082e12da249b8818cee0e30e347fe
nicolas balao <nicolasbalao.pro@gmail.com>
2024-10-17 14:58:12 +0200

    Implement basic TCP client for sending message

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,50 @@
 use std::{
-    io::{stdout, Read, Write},
+    io::{stdin, stdout, Read, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn main() -> std::io::Result<()> {
+    // Read args client / server
+
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let mode = &args[1];
+
+    match mode.as_str() {
+        "client" => {
+            client();
+        }
+        "server" => {
+            server()?;
+        }
+        _ => {
+            println!("Invalid mode");
+        }
+    }
+
+    Ok(())
+}
+
+fn client() {
+    let mut stream = TcpStream::connect("0.0.0.0:8989").expect("Failed to connect to the server");
+
+    let stdin = stdin();
+
+    loop {
+        let mut message = String::new();
+
+        print!("Message: ");
+        stdin
+            .read_line(&mut message)
+            .expect("Failed to read from stdin");
+
+        stream
+            .write(message.as_bytes())
+            .expect("Failed to write message to the streame");
+    }
+}
+
+fn server() -> std::io::Result<()> {
     // Listen on port 3425
     let listener = TcpListener::bind("0.0.0.0:8989")?;
     println!("Server listening on 0.0.0.0:8989");
@@ -12,18 +53,17 @@ fn main() -> std::io::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_client(stream);
+                handle_client_message(stream);
             }
             Err(e) => {
                 println!("Error: {}", e);
             }
         }
     }
-
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client_message(mut stream: TcpStream) {
     let mut data = [0 as u8; 50];
 
     let mut stdout = stdout();
