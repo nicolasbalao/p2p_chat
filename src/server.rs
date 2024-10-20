05efc56ad354732bd7fbd8ccff1aa3cde2c48cba
nicolas balao <nicolasbalao.pro@gmail.com>
2024-10-20 15:46:59 +0200

    feat(server): Add infinit loop for listening new client connexion

--- src/server.rs
+++ src/server.rs
@@ -7,14 +7,14 @@ pub async fn start() -> Result<(), Error> {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8989").await?;
     println!("Server listening on 0.0.0.0:8989");
 
-    let (handle, addr) = listener.accept().await?;
+    loop {
+        let (handle, addr) = listener.accept().await?;
 
-    println!("New client: {addr}");
+        println!("New client: {addr}");
 
-    let (reader, writer) = handle.into_split();
+        let (reader, writer) = handle.into_split();
 
-    read_write(reader, writer).await;
-    println!("Connection closed by the client");
-
-    Ok(())
+        read_write(reader, writer).await;
+        println!("Connection closed by the client");
+    }
 }
